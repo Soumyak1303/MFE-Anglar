1ff7fe5ea47242173f11c204e18f00ab1c3a289c
Soumyak1303 <soumyak1303@gmail.com>
2023-05-04 17:19:23 +0530

    Use Shared Library in Project Routes

--- projects/app2-restaurant/src/app/restaurant/restaurant.module.ts
+++ projects/app2-restaurant/src/app/restaurant/restaurant.module.ts
@@ -1,27 +1,36 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RestaurantComponent } from './restaurant.component';
-import { RouterModule, Routes } from '@angular/router';
-import { RestaurantDetailComponent } from './detail/detail.component';
-import { OrderComponent } from '../../../../app3-orders/src/app/order/order.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RestaurantComponent} from './restaurant.component';
+import {RouterModule, Routes} from "@angular/router";
+import {RestaurantDetailComponent} from './detail/detail.component';
+import {FileType} from "utils";
+import {mfe} from "../app-routing.module";
+import { MfeOrderComponentComponent } from './mfe-order-component/mfe-order-component.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: RestaurantComponent,
-  },
-  {
-    path: ':slug',
-    component: RestaurantDetailComponent,
-  },
-  {
-    path: ':slug/order',
-    component: OrderComponent,
-  },
-];
-
-@NgModule({
-  declarations: [RestaurantComponent, RestaurantDetailComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  const routes: Routes = [
+    {
+      path: '',
+	  component: RestaurantComponent
+    },
+    {
+      path: ':slug',
+      component: RestaurantDetailComponent
+    },
+    {
+      path: ':slug/order',
+      component: MfeOrderComponentComponent
+    }
+  ];
+  
+  @NgModule({
+  declarations: [
+    RestaurantComponent,
+    RestaurantDetailComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
 })
-export class RestaurantModule {}
+export class RestaurantModule {
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,38 +1,44 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { FileType, MfeUtil } from 'utils';
+
+export const mef = new MfeUtil();
 
 const routes: Routes = [
   {
     path: '',
-
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomeModule),
   },
-  // {
-  //   path: 'restaurants',
-  //   loadChildren: () =>
-  //     import(
-  //       '../../projects/app2-restaurant/src/app/restaurant/restaurant.module'
-  //     ).then((m) => m.RestaurantModule),
-  // },
-  // {
-  //   path: 'order',
-  //   loadChildren: () =>
-  //     import('../../projects/app3-orders/src/app/order/order.module').then(
-  //       (m) => m.OrderModule
-  //     ),
-  // },
-  // {
-  //   path: '',
-  //   redirectTo: 'home',
-  //   pathMatch: 'full',
-  // },
+  {
+    path: 'restaurants',
+    loadChildren: () =>
+      mef
+        .loadRemoteFile({
+          remoteName: 'restaurant',
+          remoteEntry: `http://localhost:4204/remoteRestaurant.js`,
+          exposedFile: 'RestaurantModule',
+          exposeFileType: FileType.Module,
+        })
+        .then((m) => m.RestaurantModule),
+  },
+  {
+    path: 'order',
+    loadChildren: () =>
+      mef
+        .loadRemoteFile({
+          remoteName: 'orders',
+          remoteEntry: `http://localhost:4205/remoteOrders.js`,
+          exposedFile: 'OrderModule',
+          exposeFileType: FileType.Module,
+        })
+        .then((m) => m.OrderModule),
+  },
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules,
       scrollPositionRestoration: 'enabled',
     }),
   ],
