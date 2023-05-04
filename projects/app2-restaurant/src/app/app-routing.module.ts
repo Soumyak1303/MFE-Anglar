539e7c8389d282cf70e471e2a2471a86fc9c2bf1
Soumyak1303 <soumyak1303@gmail.com>
2023-05-04 15:57:59 +0530

    fix routing

---
+++ projects/app2-restaurant/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'restaurants',
+    loadChildren: () => import('./restaurant/restaurant.module').then(m => m.RestaurantModule),
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,30 +3,30 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
 
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomeModule),
   },
-  {
-    path: 'restaurants',
-    loadChildren: () =>
-      import(
-        '../../projects/app2-restaurant/src/app/restaurant/restaurant.module'
-      ).then((m) => m.RestaurantModule),
-  },
-  {
-    path: 'order',
-    loadChildren: () =>
-      import('../../projects/app3-orders/src/app/order/order.module').then(
-        (m) => m.OrderModule
-      ),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+  // {
+  //   path: 'restaurants',
+  //   loadChildren: () =>
+  //     import(
+  //       '../../projects/app2-restaurant/src/app/restaurant/restaurant.module'
+  //     ).then((m) => m.RestaurantModule),
+  // },
+  // {
+  //   path: 'order',
+  //   loadChildren: () =>
+  //     import('../../projects/app3-orders/src/app/order/order.module').then(
+  //       (m) => m.OrderModule
+  //     ),
+  // },
+  // {
+  //   path: '',
+  //   redirectTo: 'home',
+  //   pathMatch: 'full',
+  // },
 ];
 
 @NgModule({
